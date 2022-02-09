8e2b1b0e268bcf06ba8806ea8decc1b82988e0db
“Robkata” <r.georgiev2017@pmg-pernik.com>
2022-02-09 13:35:14 +0200

    Add new project

---
+++ 2.DogAppFull-CRUD-без сървиси/DogApp/Abstractions/IDogService.cs
@@ -0,0 +1,17 @@
+﻿using DogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogApp.Abstractions
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, string breed, string picture);
+        bool UpdateDog(int dogId, string name, int age, string breed, string picture);
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+        Dog GetDogById(int dogId);
+        bool RemoveById(int dogId);
+    }
+}
